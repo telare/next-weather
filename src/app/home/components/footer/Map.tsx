65945732d49e8a9f029ec41f5b2ce6059d8e987e
tl <andrewzhubinskiy21@gmail.com>
2025-02-26 10:53:25 +0200

    added map component

--- src/app/home/components/footer/Map.tsx
+++ src/app/home/components/footer/Map.tsx
@@ -1,6 +1,30 @@
-import styles from "./styles/Footer.module.scss"
+"use client";
+import styles from "./styles/Footer.module.scss";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useSelector } from "react-redux";
+import { RootState } from "@/providers/global-store";
+import L from "leaflet";
 export default function Map() {
+  const { value } = useSelector((state: RootState) => state.cityName);
+  const { lat, lon } = value;
+  const customIcon = L.icon({
+    iconUrl: "/img/marker.png",
+    iconSize: [32, 32],
+  });
   return (
-    <div className={styles.left__col_map}>Map</div>
-  )
+    <div className={styles.left__col_map}>
+      <MapContainer
+        center={[Number(lat), Number(lon)]}
+        zoom={13}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker
+          position={[Number(lat), Number(lon)]}
+          icon={customIcon}
+        ></Marker>
+      </MapContainer>
+    </div>
+  );
 }
