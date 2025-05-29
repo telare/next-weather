a4848e44f6c47744a8b06a98178c3a42d63be29b
tl <andrewzhubinskiy21@gmail.com>
2025-05-29 19:58:54 +0300

    added loader skeleton for map

--- src/app/home/components/footer/Footer.tsx
+++ src/app/home/components/footer/Footer.tsx
@@ -2,10 +2,12 @@
 import dynamic from "next/dynamic";
 const WeatherMap = dynamic(() => import("./WeatherMap"), {
   ssr: false,
+  loading: () => <Skeleton className={styles.mapSection} />,
 });
 import styles from "./styles/Footer.module.scss";
 import TopRatedCities from "./TopRatedCities";
 import WeekForecast from "./WeekForecast";
+import Skeleton from "@/shared/components/Skeletons/Skeleton";
 export default function Footer() {
   return (
     <div className={styles.footerCon}>
