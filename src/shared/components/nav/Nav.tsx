e058d93cf64830c1ec8eb79afec85247bad7fd78
tl <andrewzhubinskiy21@gmail.com>
2025-05-30 21:29:00 +0300

    nav bar adaptive

--- src/shared/components/nav/Nav.tsx
+++ src/shared/components/nav/Nav.tsx
@@ -5,11 +5,18 @@ import LogOutBtn from "../btns/LogOutBtn";
 import Links from "./Links";
 import styles from "@shared/styles/Nav.module.scss";
 import GitBtn from "../btns/GitBtn";
+import Hamburger from "hamburger-react";
+import { useState } from "react";
+
 export default function Nav() {
+  const [isOpen, setOpen] = useState<boolean>(false);
   return (
     <div className={styles.navCon} tabIndex={0}>
       <Links />
-      <div className={styles.navContent}>
+      <div className={styles.hamburgerLogo}>
+        <Hamburger toggled={isOpen} toggle={setOpen} />
+      </div>
+      <div className={styles.navContent} aria-expanded={isOpen}>
         <Search />
         <div className={styles.navContentBtns}>
           <ThemeToggleBtn />
