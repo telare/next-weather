b1c2ed3c5ee86ae3ec7942bf76de25f552b27875
tl <andrewzhubinskiy21@gmail.com>
2025-06-29 12:57:36 +0300

    fix: create a dedicated file for storing cypress env chore: add first workflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,13 +5,7 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     fixturesFolder: "cypress/e2e/fixtures",
-    env: {
-      JWT_SECRET_KEY:
-        "b79f66f7f6d8dd4071ff135df116e5aaf65c34369b8c053e1fa1854bab1f7592",
-    },
     setupNodeEvents(on, config) {
-      config.env.JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
-
       const prisma = new PrismaClient();
 
       on("task", {
